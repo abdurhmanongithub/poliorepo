53ebc3c42c9049128d7d950e163ca2ac2d6f3a87
Abdurhman <findabdurhman@gmail.com>
2024-12-28 12:09:23 +0300

    Gps corelation removed

--- resources/views/core-group-data/charts/index.blade.php
+++ resources/views/core-group-data/charts/index.blade.php
@@ -2,7 +2,7 @@
 @include('core-group-data.charts.missing-data')
 @endif
 @include('core-group-data.charts.regional_distribution')
-@include('core-group-data.charts.gps-corelation')
+{{-- @include('core-group-data.charts.gps-corelation') --}}
 @include('core-group-data.charts.get-case-counts')
 
 @include('core-group-data.charts.core-map-chart')
