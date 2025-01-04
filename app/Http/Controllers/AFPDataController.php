8490a9a306114ae5b00234af7c83a7b11c2c4e94
Abdurhman <findabdurhman@gmail.com>
2025-01-04 14:15:53 +0300

    dashboard fix

--- app/Http/Controllers/AFPDataController.php
+++ app/Http/Controllers/AFPDataController.php
@@ -243,6 +243,12 @@ class AFPDataController extends Controller
             ];
         }
         $result = Constants::sendGeezBulkSms($contacts, $content->content);
+
+        if (!$result['error']) {
+            foreach ($broadcasts as $broadcast) {
+                $broadcast->update(['status' => Constants::BROADCAST_STATUS_SENT]);
+            }
+        }
         return redirect()->back()->with('success', 'Broadcast send successfully');
     }
 
